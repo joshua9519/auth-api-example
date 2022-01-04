4afb871bce32eab95e9af7932bf6c3dae5304bcf
Josh Hill <josh.hill@cts.co>
2022-01-04 16:28:54 +0000

    Initial commit of API and client

---
+++ client/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"log"
+
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/option"
+)
+
+// makeIAPRequest makes a request to an application protected by Identity-Aware
+// Proxy with the given audience.
+func makeIAPRequest(w io.Writer, request *http.Request) error {
+	// request, err := http.NewRequest("GET", "http://example.com", nil)
+	audience := "406810047214-jc75s22od84i0nbgolajp5nt7kco8cee.apps.googleusercontent.com"
+	ctx := context.Background()
+
+	// client is a http.Client that automatically adds an "Authorization" header
+	// to any requests made.
+	client, err := idtoken.NewClient(ctx, audience, option.WithCredentialsFile("./azure-auth.json"))
+	if err != nil {
+			return fmt.Errorf("idtoken.NewClient: %v", err)
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+			return fmt.Errorf("client.Do: %v", err)
+	}
+	defer response.Body.Close()
+	if _, err := io.Copy(w, response.Body); err != nil {
+			return fmt.Errorf("io.Copy: %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	request, err := http.NewRequest("GET", "https://api.josh.cts-gcp.com/ping", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Add("Accept", "application/json")
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n\n", body)
+
+	if err = makeIAPRequest(os.Stdout, request); err != nil {
+		log.Fatal(err)
+	}
+	println()
+}
